c8f3b8dda1b6ad3238ac6f0f754118a00962a404
chroming <guangzhishang@gmail.com>
2017-01-16 00:43:59 +0800

    界面组件大小及位置

---
+++ ui_conf.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+"""
+界面组件的大小和位置
+"""
+
+maincenter = {
+    'code_text': (1.2, 0.9),
+    'name_text': (0.7, 1.3)
+}
+
+mainsize = {
+    'code_text': (1, 1),
+    'name_text': (0.5, 0.5)
+}
+
+inputcenter = {
+    'name_label': (1.2, 0.9),
+    'name_textfield': (1.2, 1),
+    'code_label': (1.2, 1.2),
+    'code_textfield': (1.2, 1.3),
+    'ok_button': (3, 0.9),
+    'cancel_button': (3.2, 0.9)
+}
+
+inputsize = {
+    'name_label': (1, 1),
+    'name_textfield': (1, 1),
+    'code_label': (1, 1),
+    'code_textfield': (1, 1),
+    'ok_button': (1, 1),
+    'cancel_button': (1, 1)
+}
