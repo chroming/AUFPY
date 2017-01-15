62df72af2db1e2b5aee67a871cf2b8600c4e7380
chroming <guangzhishang@gmail.com>
2017-01-16 00:44:32 +0800

    新增 textfield 和 button

--- ui_class.py
+++ ui_class.py
@@ -6,12 +6,34 @@ Pythonista 的 ui 类无法继承，只能写成函数形式
 import ui
 
 
-def Nlabel(view):
+def ntextfield(view):
+    textfield = ui.TextField()
+
+    textfield.border_color = 'black'
+    textfield.border_width = 2
+
+    textfield.width = view.width * 4
+    textfield.height = view.height * 0.3
+
+    return textfield
+
+
+def nlabel(view):
     label = ui.Label()
-    label.border_color = 'black'
-    label.border_width = 2
 
     label.width = view.width * 2
     label.height = view.height * 0.5
 
     return label
+
+
+def nbutton(view):
+    button = ui.Button()
+
+    button.border_color = 'black'
+    button.border_width = 2
+
+    button.width = view.width * 0.7
+    button.height = view.height * 0.5
+
+    return button
