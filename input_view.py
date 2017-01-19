111ae739907e61d70fdfc1188b57cb99d62e19ca
chroming <guangzhishang@gmail.com>
2017-01-20 00:54:35 +0800

    Add notification after add success

--- input_view.py
+++ input_view.py
@@ -2,13 +2,13 @@
 from ui_class import *
 from ui_conf import inputcenter, inputsize
 from conn_sqlite import *
-
+import notification
 
 class InputView(ui.View):
     def __init__(self):
         self.name = "INPUT NAME & CODE"
         self.background_color = 'white'
-        self.conn = ConnSqlite()
+        # self.conn = ConnSqlite()
         self.present('123')
         self._init_view()
 
@@ -22,18 +22,18 @@ class InputView(ui.View):
 
         self.secret_label = nlabel(self)
         self.secret_label.center = (self.width * inputcenter['code_label'][0], self.height * inputcenter['code_label'][1])
-        self.secret_label.text = "SECRET_CODE"
+        self.secret_label.text = "SECRET"
         self.secret_textfield = ntextfield(self)
         self.secret_textfield.center = (self.width * inputcenter['code_textfield'][0],
                                         self.height * inputcenter['code_textfield'][1])
 
         self.ok_button = nbutton(self)
-        self.ok_button.text = 'OK'
+        self.ok_button.title = 'OK'
         self.ok_button.center = (self.width * inputcenter['ok_button'][0], self.height * inputcenter['ok_button'][1])
         self.ok_button.action = self.ok_tapped
 
         self.cancel_button = nbutton(self)
-        self.cancel_button.text = 'CANCEL'
+        self.cancel_button.title = 'CANCEL'
         self.cancel_button.center = (self.width * inputcenter['cancel_button'][0],
                                      self.height * inputcenter['cancel_button'][1])
         self.cancel_button.action = self.cancel_tapped
@@ -51,14 +51,16 @@ class InputView(ui.View):
         :param sender:
         :return: True
         """
+        self.conn = ConnSqlite()
         name_text = self.name_textfield.text
         secret_text = self.secret_textfield.text
         if name_text and secret_text:
             self.conn.insert_value(name_text, secret_text)
-            return True
+            notification.schedule('Add success!', 1)
+        self.conn.close_conn()
 
     def cancel_tapped(self, sender):
-        return False
+        self.close()
 
 if __name__ == '__main__':
     iui = InputView()
