f913078e3ebe30aa5228e185213612f781d5fd88
chroming <guangzhishang@gmail.com>
2017-03-04 18:15:08 +0800

    Add countdown and autoreload

--- main_table.py
+++ main_table.py
@@ -11,9 +11,18 @@ def _show_all_texts(code_texts, all_accounts):
     codes_accounts = [(code_texts[x], all_accounts[x]) for x in range(len_c)]
     main_ui.data_source = ui.ListDataSource(['%s -- %s' % (x, y) for x, y in codes_accounts])
     main_ui.present()
+    auto_reload()
 
 
-def _get_all_codes(name_texts):
+def reload_all_texts(code_texts, all_accounts):
+    len_c = len(code_texts)
+    codes_accounts = [(code_texts[x], all_accounts[x]) for x in range(len_c)]
+    main_ui.data_source = ui.ListDataSource(['%s -- %s' % (x, y) for x, y in codes_accounts])
+    main_ui.reload()
+
+
+def _get_all_codes():
+    global countdown
     """
     根据名称 list 获取 code_texts 列表并调用 _show_all_texts
     :param name_texts: 名称 list
@@ -21,18 +30,20 @@ def _get_all_codes(name_texts):
     """
     code_texts = []
     for c in name_texts:
-        code_text, _ = get_code(conn.select_code(c)[0])
+        code_text, countdown = get_code(conn.select_code(c)[0])
         code_texts.append(code_text)
     _show_all_texts(code_texts, name_texts)
 
 
 def get_all_accounts():
+    global name_texts
     """
     从sqlite 中获取所有名称 并调用get_texts
     :return: None
     """
     names = conn.select_all()
-    _get_all_codes([name[0] for name in names])
+    name_texts = [name[0] for name in names]
+    _get_all_codes()
 
 
 def add_button_tapped(sender):
@@ -54,12 +65,28 @@ def _save_to_db(new_dict):
         notification.schedule('Add success!', 1)
     conn.close_conn()
 
+
+def auto_reload():
+    global countdown
+    while main_ui.on_screen:
+        countdown -= 1
+        if countdown == 0:
+            _get_all_codes()
+            break
+        else:
+            time_button_item.title = '%s' % str(countdown)
+            main_ui.reload()
+            time.sleep(1)
+
+countdown = '*'
 main_ui = ui.TableView()
 main_ui.name = 'AUFPY'
 add_button_item = ui.ButtonItem()
 add_button_item.title = 'ADD'
 add_button_item.action = add_button_tapped
-main_ui.right_button_items = [add_button_item]
+time_button_item = ui.ButtonItem()
+time_button_item.title = '%s' % countdown
+main_ui.right_button_items = [add_button_item, time_button_item]
 
 conn = ConnSqlite()
 get_all_accounts()
